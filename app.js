f0a297443568c6392ff572405d128499bf5d82bd
RobinOShiaa <oshea.robinho@gmail.com>
2022-07-30 21:26:56 +0100

    change

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 const $albumTemplate = document.getElementById('album-template').innerHTML;
 const $mainView = document.querySelector('.row');
+const $filterSearch = document.getElementById('search-example');
 
-
-const setUp = async () => {
+document.addEventListener('DOMContentLoaded', async (e) => {
   const alb = new Albums();
-  const result = await alb.getAll();
-  Object.keys(result).map(id => {
-    const html = Mustache.render($albumTemplate, {
-      title : result[id].title,
-      thumbnailUrl : result[id].content[0].thumbnailUrl
-    });
-    $mainView.insertAdjacentHTML('beforeend',html) 
+  const dataResult = await alb.getAll();
+  paint(dataResult);
+
+});
+
+$filterSearch.addEventListener('keyup', (e) => {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.card-title').forEach(title => {
+    const item = title.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      title.parentElement.parentElement.style.display = 'block';
+    } else {
+      title.parentElement.parentElement.style.display = 'none';
+    }
   });
-  
-  console.log(JSON.stringify (result, null, " "));
-  // Object.keys(result).forEach(item => localStorage.setItem('photos',JSON.stringify(result[item].content)));
+});
+
+const paint = (data) => {
+  try {
+    Object.keys(data).map(id => {
+      const html = Mustache.render($albumTemplate, {
+        title : data[id].title,
+        thumbnailUrl : data[id].content[0].thumbnailUrl
+      });
+      $mainView.insertAdjacentHTML('beforeend',html) 
+    });
+  }
+  catch(e) {
+    throw new Error(e);
+  }
 };
 
-setUp();
-
-
+function filterTasks(e) {
+ 
+}
--- web-album.js
+++ web-album.js
@@ -1,9 +1,8 @@
+const ALBUM_URL =  "https://jsonplaceholder.typicode.com/albums";
+const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos';
+
 class Albums {
-
   getAll() {
-    const ALBUM_URL = "https://jsonplaceholder.typicode.com/albums";
-    const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos';
-
     return new Promise(async (resolve,reject) => {
       const albResp =  await fetch(ALBUM_URL);
       let albums = await albResp.json();
@@ -11,20 +10,18 @@ class Albums {
       const phoResp =  await fetch(PHOTOS_URL);
       const photos = await phoResp.json();
 
-
-      albums = albums.reduce((acc, curr) => {
+      let dataResult = albums.reduce((acc, curr) => {
         if (acc[curr.id] === undefined) {
           acc[curr.id] = { userId: curr.userId, title: curr.title, content: [] };
         }
         return acc;
       }, {});
-      photos.forEach(photo => albums[photo.albumId].content.push(photo));
+      photos.forEach(photo => dataResult[photo.albumId].content.push(photo));
       try {
-        resolve(albums);
+        resolve(dataResult);
       } catch(error) {
         reject(error)
       }
     }) 
   } 
 }
-

39b6bb4561b4ec3d8a4cc95cdae22f880832e574
RobinOShiaa <oshea.robinho@gmail.com>
2022-07-31 01:11:03 +0100

    finished rendering photos

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const $albumTemplate = document.getElementById('album-template').innerHTML;
 const $mainView = document.querySelector('.row');
 const $filterSearch = document.getElementById('search-example');
-
+const $photosTemplate = document.getElementById('photo-template').innerHTML;
 
 let viewPhotos = false;
 const alb = new Albums();
 
 document.addEventListener('DOMContentLoaded', async (e) => {
   const dataResult = await alb.getAll();
-  paint(dataResult);
+  paintAlbums(dataResult);
 
 });
 
@@ -16,15 +16,12 @@ document.addEventListener('DOMContentLoaded', async (e) => {
 $mainView.addEventListener('click', async (e) => {
   if (e.target.id === 'view-button') {
     viewPhotos = true;
-    const name = e.target.parentElement.previousElementSibling.children[1].textContent;
-    console.log(name)
+    const albumID = e.target.nextElementSibling.textContent;
+    console.log(albumID);
     $mainView.innerHTML = '';
-    // const dataResult = await alb.getAll();
-    // const photos = dataResult
-   
-      
-    // })
-    // console.log(photos)
+    const dataResult = await alb.getAll();
+    const photos = dataResult[albumID].content;
+    paintPhotos(photos);
 
   }
 
@@ -44,12 +41,13 @@ $filterSearch.addEventListener('keyup', (e) => {
   });
 });
 
-const paint = (data) => {
+const paintAlbums = (data) => {
   try {
     Object.keys(data).map(id => {
       const html = Mustache.render($albumTemplate, {
         title : data[id].title,
-        thumbnailUrl : data[id].content[0].thumbnailUrl
+        thumbnailUrl : data[id].content[0].thumbnailUrl,
+        id : id
       });
       $mainView.insertAdjacentHTML('beforeend',html) 
     });
@@ -59,3 +57,19 @@ const paint = (data) => {
   }
 };
 
+
+const paintPhotos = (data) => {
+  try {
+    data.forEach(item => {
+      const html = Mustache.render($photosTemplate, {
+        title : item.title,
+        thumbnailUrl : item.thumbnailUrl
+      });
+      $mainView.insertAdjacentHTML('beforeend',html) 
+
+    })
+  }
+  catch(e) {
+    throw new Error(e);
+  }
+};

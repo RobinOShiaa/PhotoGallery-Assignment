519815c0e2cf7b2874d8251f18ee7a3a73e2dbcb
RobinOShiaa <oshea.robinho@gmail.com>
2022-08-01 16:32:37 +0100

    adding comments

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
+// Album & Photo template with script tag to be dynamically generated using Mustache
 const $albumTemplate = document.getElementById('album-template').innerHTML;
-const $mainView = document.querySelector('.row');
-const $filterSearch = document.getElementById('search-example');
 const $photosTemplate = document.getElementById('photo-template').innerHTML;
+// Main element for all dynamic html elements to be appended inside
+const $mainView = document.querySelector('.row');
+// Search button
+const $filterSearch = document.getElementById('search-example');
+// Button to return to albums 
 const $backButton = document.getElementsByClassName('back-button');
 
 let viewPhoto = false;
 const alb = new Albums();
 
-
-
+// Event listener on the startup of the webpage
 document.addEventListener('DOMContentLoaded', async () => {
   const dataResult = await alb.getAll();
+  // store data set inside session storage to be referenced later throughout the code
   sessionStorage.setItem('data',JSON.stringify(dataResult));
   console.log(JSON.parse(sessionStorage.getItem('data', dataResult)));
   paintAlbums(dataResult);
 });
 
-
-
 $mainView.addEventListener('click', (e) => {
   switch(e.target.id) {
     case 'view-button' :
+      // Retrieve data set
       const dataResult = JSON.parse(sessionStorage.getItem('data'));
       const albumName = e.target.parentElement.previousElementSibling.children[1].textContent;
       const albumID = e.target.nextElementSibling.textContent;
+      // Set heading of corresponding photos viewed
       $mainView.innerHTML = 
               `<div style="display: flex; align-items: center;">
                 <i id="back-button" class="medium material-icons cursor arrow" style="color: #212121; width: 60px;">arrow_back</i>
@@ -34,7 +38,6 @@ $mainView.addEventListener('click', (e) => {
       break;
     
     case 'back-button':
-      
       paintAlbums(JSON.parse(sessionStorage.getItem('data')));
       viewPhotos = false;
       break;
@@ -53,35 +56,36 @@ $mainView.addEventListener('click', (e) => {
       }
       break;
 
-
     case 'download':
       if(confirm(`You are about to leave website`)) {
         fetchFile(e.target.parentElement.parentElement.previousElementSibling.previousElementSibling.src)
       }
-
-
   }
 
 });
 
 const fetchFile = (url) => {
   window.location.assign(url);
-
 }
 
+// Checks the main container for char entries that exist on the page
 $filterSearch.addEventListener('keyup', (e) => {
   const text = e.target.value.toLowerCase();
-
   document.querySelectorAll('.card-title').forEach(title => {
+    // (album || photo) name
     const item = title.textContent;
     if(item.toLowerCase().indexOf(text) != -1){
+      // show card of photo || album
       title.parentElement.parentElement.style.display = 'block';
     } else {
+      // dont show card of photo || album
       title.parentElement.parentElement.style.display = 'none';
     }
   });
 });
 
+
+// Using mustache fill in relevant data inside the main view using the album templates
 const paintAlbums = (data) => {
   $mainView.innerHTML = '<h1>Albums</h1>';
   try {
@@ -99,6 +103,7 @@ const paintAlbums = (data) => {
   }
 };
 
+// Using mustache fill in relevant data inside the main view using the photo templates
 
 const paintPhotos = (data) => {
   try {
@@ -108,7 +113,6 @@ const paintPhotos = (data) => {
         thumbnailUrl : item.thumbnailUrl
       });
       $mainView.insertAdjacentHTML('beforeend',html) 
-
     })
   }
   catch(e) {
